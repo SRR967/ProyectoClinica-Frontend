e431b0e0deb8e1a925611b110da6e53115eba5f4
Santiago Ramon <santiramon967gmail.com>
2023-10-29 17:54:30 -0500

    Creacion estructurar basica del proyecto.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { LoginComponent } from './pagina/login/login.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: InicioComponent},
+  {path: "login", component: LoginComponent},
+  {path: "registro", component: RegistroComponent},
+  {path: "*", pathMatch: "full", redirectTo: ""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
